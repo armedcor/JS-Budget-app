9099c9ac649c0c1ccae996bfe235c44fd162e3e1
Cormac Lawlor <armedcor@gmail.com>
2020-09-13 22:46:36 +0100

    implement input data reading

--- static/app.js
+++ static/app.js
@@ -8,21 +8,43 @@ var budgetController = (function () {
 
 
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
 
-    // Some Code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
 
+  };
+
+  return {
+    getinput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+      getDOMstrings: function() {
+        return DOMstrings;
+      }
+  };
 })();
 
 
 
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-  var ctrlAddItem = function() {
+  var DOM = UICtrl.getDOMstrings();
 
+  var ctrlAddItem = function () {
     // 1. Get the field input data
+
+    var input = UIController.getinput();
+    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -31,19 +53,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
+  };
 
-    console.log('it works!');
-  }
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    
+  document.addEventListener("keypress", function (event) {
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
   });
-
 })(budgetController, UIController);
 
 
