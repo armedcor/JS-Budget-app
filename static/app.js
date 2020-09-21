cbff96aab35cbf15f28314cb987be952dfc01a5b
Cormac Lawlor <armedcor@gmail.com>
2020-09-21 22:24:10 +0100

    add new item to budget controller

--- static/app.js
+++ static/app.js
@@ -23,7 +23,37 @@ var budgetController = (function () {
       exp: 0,
       inc: 0
     }
-  }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      
+      // Create new item based on 'inc' or 'exp# type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+      // Push is into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
   
 
 
@@ -52,6 +82,20 @@ var UIController = (function () {
         value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
+
+    addListItem: function(obj, type) {
+      
+      // Create HTML string with placeholder text
+
+
+      // Replace the placeholder text with actual data
+
+
+      // Insert the HTML into the DOM
+      
+    },
+
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -78,11 +122,12 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   
   var ctrlAddItem = function () {
+    var input, newItem;
     // 1. Get the field input data
-    var input = UIController.getinput();
+    input = UICtrl.getinput();
 
     // 2. Add the item to the budget controller
-
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
 
     // 4. Calculate the budget
